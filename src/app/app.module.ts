3ae4c489a64c1d8674b62b3125ed3d4ffc40e708
Bhavic22 <54400173+Bhavic22@users.noreply.github.com>
2020-04-30 15:12:09 +0200

    Create event-event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavigationComponent } from './common/navigation/navigation.component';
 import { HomeViewComponent } from './home-view/home-view.component';
+import { EventViewComponent } from './event-view/event-view.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, HomeViewComponent],
+  declarations: [AppComponent, NavigationComponent, HomeViewComponent, EventViewComponent],
   imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
